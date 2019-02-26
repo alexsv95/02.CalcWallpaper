27c1db8e091a05db6b134c06609d4313d2ffde41
alexsv95 <a.sosnov95@gmail.com>
2019-02-26 11:56:18 +0300

    Edit line 7

--- src/ru/itpark/Main.java
+++ src/ru/itpark/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         CalcWallPaper service = new CalcWallPaper();
-        System.out.println(service.calculate(5,6,2.75));
+        System.out.println("Для оклейки комнаты необходимо " + service.calculate(5,6,2.75) + " рулонов обоев");
     }
 }
