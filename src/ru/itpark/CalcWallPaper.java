4961249b01f3f1a0ae1bb46895957de5b18231f0
alexsv95 <a.sosnov95@gmail.com>
2019-03-11 14:50:10 +0300

    Renamed variables,create new variable.

--- src/ru/itpark/CalcWallPaper.java
+++ src/ru/itpark/CalcWallPaper.java
@@ -1,9 +1,13 @@
 package ru.itpark;
 
 public class CalcWallPaper {
-    public double calculate(double longWall, double longWall2, double ceilingHeight) {
-        double stripWallpaper = ((longWall + longWall2) * 2) / 1;
-        double canvasWallpaper = 10 / (ceilingHeight + 0.10);
+    public double calculate(double wallWidth, double wallLength, double ceilingHeight) {
+
+        int lengthWallpaperRoll = 10; // Длина обойного рулона = 10м
+        double additionalStockCeiling = 0.10; // Дополнительный запас к высоте потолка
+        double widthWallpaper = 1.06;
+        double stripWallpaper = ((wallLength + wallWidth) * 2) / widthWallpaper;
+        double canvasWallpaper = lengthWallpaperRoll / (ceilingHeight + additionalStockCeiling);
         int summWallpaper = (int) (stripWallpaper / canvasWallpaper) + 1;
         return summWallpaper;
     }
